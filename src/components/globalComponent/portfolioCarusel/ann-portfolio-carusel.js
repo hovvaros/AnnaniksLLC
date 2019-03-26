4b9b69ecf5724ddf334da41052f1dc441a50afb7
rado <rado.poghossian0398@gmail.com>
2019-03-26 20:05:11 +0400

    portfolio-slider

--- src/components/globalComponent/portfolioCarusel/ann-portfolio-carusel.js
+++ src/components/globalComponent/portfolioCarusel/ann-portfolio-carusel.js
@@ -1,130 +1,26 @@
-// import { VueperSlides, VueperSlide } from "vueperslides";
-// import SliderItem from "./../portfolioItem/ann-portfolio-item.vue";
-// import "vueperslides/dist/vueperslides.css";
+$(document).ready(function(){
 
-// export default {
-//   components: {
-//     "ann-portfolio-item": SliderItem
-//   },
-//   data: () => ({
+    $(".filter-button").click(function(){
+        var value = $(this).attr('data-filter');
+        
+        if(value == "all")
+        {
+            //$('.filter').removeClass('hidden');
+            $('.filter').show('1000');
+        }
+        else
+        {
+//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+            $(".filter").not('.'+value).hide('3000');
+            $('.filter').filter('.'+value).show('3000');
+            
+        }
+    });
     
-//     slides: [
-//       {
-//         title: "Slide #1",
-//         content: `<div class=" slider_item width_80_auto" >
-//         <div class="slider_item_top displey_flex_flex_direcshon_column_center">
-//         <div class="slider_item_top_text margin_left_7"><span class="lorem_ispum ">
-//         LOREM IPSUM DOLOR
-//         </span>
-//         </div>
-//         <div class="webpage_container">
-//         <div class="slider_item_bottom_icon margin_left_7">
-//         <span class=""><i class="icon-globe"></i></span>
-//         </div><div class="slider_item_bottom_text">
-//         <span class="margin_left_30">webpage</span>
-//         </div></div></div><div class="slider_item_bottom">
-//         </div>
-//         </div>`
-//       },
-//       {
-//         title: "Slide #1",
-//         content: `<div class=" slider_item width_80_auto" >
-//         <div class="slider_item_top displey_flex_flex_direcshon_column_center">
-//         <div class="slider_item_top_text margin_left_7"><span class="lorem_ispum ">
-//         LOREM IPSUM DOLOR
-//         </span>
-//         </div>
-//         <div class="webpage_container">
-//         <div class="slider_item_bottom_icon margin_left_7">
-//         <span class=""><i class="icon-globe"></i></span>
-//         </div><div class="slider_item_bottom_text">
-//         <span class="margin_left_30">webpage</span>
-//         </div></div></div><div class="slider_item_bottom">
-//         </div>
-//         </div>`
-//       },
-//       {
-//         title: "Slide #1",
-//         content: `<div class=" slider_item width_80_auto" >
-//         <div class="slider_item_top displey_flex_flex_direcshon_column_center">
-//         <div class="slider_item_top_text margin_left_7"><span class="lorem_ispum ">
-//         LOREM IPSUM DOLOR
-//         </span>
-//         </div>
-//         <div class="webpage_container">
-//         <div class="slider_item_bottom_icon margin_left_7">
-//         <span class=""><i class="icon-globe"></i></span>
-//         </div><div class="slider_item_bottom_text">
-//         <span class="margin_left_30">webpage</span>
-//         </div></div></div><div class="slider_item_bottom">
-//         </div>
-//         </div>`
-//       },
-//       {
-//         title: "Slide #1",
-//         content: `<div class=" slider_item width_80_auto" >
-//         <div class="slider_item_top displey_flex_flex_direcshon_column_center">
-//         <div class="slider_item_top_text margin_left_7"><span class="lorem_ispum ">
-//         LOREM IPSUM DOLOR
-//         </span>
-//         </div>
-//         <div class="webpage_container">
-//         <div class="slider_item_bottom_icon margin_left_7">
-//         <span class=""><i class="icon-globe"></i></span>
-//         </div><div class="slider_item_bottom_text">
-//         <span class="margin_left_30">webpage</span>
-//         </div></div></div><div class="slider_item_bottom">
-//         </div>
-//         </div>`
-//       },
-//        {
-//         title: "Slide #1",
-//         content: `<div class=" slider_item width_80_auto" >
-//         <div class="slider_item_top displey_flex_flex_direcshon_column_center">
-//         <div class="slider_item_top_text margin_left_7"><span class="lorem_ispum ">
-//         LOREM IPSUM DOLOR
-//         </span>
-//         </div>
-//         <div class="webpage_container">
-//         <div class="slider_item_bottom_icon margin_left_7">
-//         <span class=""><i class="icon-globe"></i></span>
-//         </div><div class="slider_item_bottom_text">
-//         <span class="margin_left_30">webpage</span>
-//         </div></div></div><div class="slider_item_bottom">
-//         </div>
-//         </div>`
-//       }
+    if ($(".filter-button").removeClass("active")) {
+$(this).removeClass("active");
+}
+$(this).addClass("active");
 
-//       // {
-//       //   title: "Slide #2",
-//       //   content: `<ann-portfolio-item> </ann-portfolio-item>`
-//       // }
-//     ],
-//     window: {
-//       width: 0,
-//       height: 0
-//     }
-//   }),
-
-//   components: { VueperSlides, VueperSlide, props: true },
-//   created() {
-//     window.addEventListener("resize", this.handleResize);
-//     this.handleResize();
-//   },
-
-//   methods: {
-//     handleResize() {
-//       this.window.width = window.innerWidth;
-//       this.window.height = window.innerHeight;
-//     }
-//   },
-//   computed: {
-//     visible() {
-//       if (this.window.width <= 1300) {
-//         return 1;
-//       } else {
-//         return 4;
-//       }
-//     }
-//   }
-// };
+});
--- src/components/globalComponent/portfolioCarusel/ann-portfolio-carusel.vue
+++ src/components/globalComponent/portfolioCarusel/ann-portfolio-carusel.vue
@@ -1,6 +1,62 @@
 <template>
   <div class="mb233">
-   
+    <div class="container">
+      <div class="row">
+        <div align="center" class="portfolioButtonContainer">
+          <button class="btn btn-default filter-button" data-filter="all">All</button>
+          <button class="btn btn-default filter-button" data-filter="hdpe">HDPE Pipes</button>
+          <button class="btn btn-default filter-button" data-filter="sprinkle">Sprinkle Pipes</button>
+          <button class="btn btn-default filter-button" data-filter="spray">Spray Nozzle</button>
+          <button class="btn btn-default filter-button" data-filter="irrigation">Irrigation Pipes</button>
+        </div>
+        <br>
+
+        <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter hdpe">
+          <img src="http://fakeimg.pl/365x365/" class="img-responsive">
+          <div  class="slider_item width_80_auto img-responsive">
+            <div class="slider_item_top displey_flex_flex_direcshon_column_center">
+              <div class="slider_item_top_text margin_left_7">
+                <span class="lorem_ispum">
+                  <!-- {{portfolioInfo.topName}}  -->
+
+                  LOREM IPSUM DOLOR
+                </span>
+              </div>
+              <div class="webpage_container">
+                <div class="slider_item_bottom_icon margin_left_7">
+                  <span class>
+                    <i class="icon-globe"></i>
+                  </span>
+                </div>
+                <div class="slider_item_bottom_text">
+                  <span class="margin_left_30">
+                    <!-- {{ portfolioInfo.description }} -->
+                    webpage
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div class="slider_item_bottom"></div>
+          </div>
+        </div>
+
+        <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter sprinkle">
+          <img src="http://fakeimg.pl/365x365/" class="img-responsive">
+        </div>
+
+        <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter hdpe">
+          <img src="http://fakeimg.pl/365x365/" class="img-responsive">
+        </div>
+
+        <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter irrigation">
+          <img src="http://fakeimg.pl/365x365/" class="img-responsive">
+        </div>
+
+        <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter spray">
+          <img src="http://fakeimg.pl/365x365/" class="img-responsive">
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
